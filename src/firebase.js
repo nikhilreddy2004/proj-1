e8e285fc1aa12ef82a4e859157fa92c300468f6a
MuskulaNikhilReddy <nikhireddy2004@gmail.com>
2025-06-24 17:22:29 +0530

    feat: Secure Firebase credentials using environment variables

--- src/firebase.js
+++ src/firebase.js
@@ -5,12 +5,12 @@ import { getStorage } from "firebase/storage";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAEonQyS-jP2Z4XM6OOxzeo_rOagX-U_dk",
-  authDomain: "ngo-mvp-app.firebaseapp.com",
-  projectId: "ngo-mvp-app",
-  storageBucket: "ngo-mvp-app.firebasestorage.app",
-  messagingSenderId: "774363048476",
-  appId: "1:774363048476:web:0b5885f21853c62d8ccdd9"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
 
 // Initialize Firebase
